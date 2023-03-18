b3db3124c2256ac78962cf70af7cbc9a37eeb389
Martin Cutillo-Rigby <70958005+mcrigby@users.noreply.github.com>
2023-03-18 16:01:05 +0000

    revert harness to basic test of gamepad

--- Harness/Program.cs
+++ Harness/Program.cs
@@ -1,5 +1,4 @@
-﻿using System.Device.Gpio;
-using CutilloRigby.Input.Gamepad;
+﻿using CutilloRigby.Input.Gamepad;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -29,20 +28,10 @@ class Program
                 var gamepadSettingsSection = hostBuilder.Configuration.GetSection("GamepadSettings");
                 var gamepadSettingsConfiguration = gamepadSettingsSection.Get<GamepadSettingsConfiguration>(options => options.ErrorOnUnknownConfiguration = true);
 
-                //services.AddSingleton<IServoSettings>(provider =>
-                //    provider.GetRequiredService<GamepadState>()
-                //);
-
                 services.AddGamepadState(gamepadSettingsConfiguration.Name, gamepadSettingsConfiguration.DeviceFile,
                     gamepadSettingsConfiguration.Axes, gamepadSettingsConfiguration.Buttons);
                     
                 services.AddGamepadController();
-
-                services.AddSingleton<GpioController>();
-                services.AddSingleton<StatusLed>();
-
-                services.AddHostedService<Steering_Servo>();
-                services.AddHostedService<TBLE01_ESC>();
             })
             .Build();
 
@@ -54,8 +43,6 @@ class Program
 
         await host.StartAsync(lifetime.ApplicationStopping);
         await host.WaitForShutdownAsync(lifetime.ApplicationStopped);
-
-        Stop(host);
     }
 
     private static void Start(IHost? host)
@@ -64,49 +51,16 @@ class Program
             return;
 
         var gamepadInputChanged = host.Services.GetRequiredService<IGamepadInputChanged>();
-        var statusLed = host.Services.GetRequiredService<StatusLed>();
+        var gamepadAvailable = host.Services.GetRequiredService<IGamepadAvailable>();
+
+        gamepadAvailable.AvailableChanged += (s, e) => 
+            Console.WriteLine($"Gamepad Availability Changed: {e.Value}");
 
         // Configure this if you want to get events when the state of a button changes
-        gamepadInputChanged.ButtonChanged += (object? sender, GamepadButtonInputEventArgs e) =>
-        {
-            statusLed.SetBlueLed(true);
-        
-            //if (e.Address == 3 && !e.Value && braking == DrivingMode.Braking)
-            //{
-            //    rcCarHat.SetDrive(0);
-            //    await Task.Delay(800);
-            //    braking = DrivingMode.ForwardOnly;
-            //    statusLed.SetGreenLed(false);
-            //    rcCarHat.SetDrive(MuxLTandRT(gamepadSettings.GetAxis(5), gamepadSettings.GetAxis(4)));
-            //}
-            //else if (e.Address == 3 && e.Value)
-            //{
-            //    braking = DrivingMode.Braking;
-            //
-            //    statusLed.SetGreenLed(true);
-            //
-            //    if(MuxLTandRT(gamepadSettings.GetAxis(5), gamepadSettings.GetAxis(4)) > TBLE01_Deadband_Upper)
-            //        rcCarHat.SetDrive(short.MinValue);
-            //    else
-            //        rcCarHat.SetDrive(0);
-            //}
-            //else
-                Console.WriteLine($"Button {e.Name} ({e.Address}) Changed: {e.Value}");
-        
-            statusLed.SetBlueLed(false);
-        };
+        gamepadInputChanged.ButtonChanged += (s, e) =>
+            Console.WriteLine($"Button {e.Name} ({e.Address}) Changed: {e.Value}");
         // Configure this if you want to get events when the state of an axis changes
-    }
-
-    private static void Stop(IHost? host)
-    {
-        if (host == null)
-            return;
-
-        var statusLed = host.Services.GetRequiredService<StatusLed>();
-
-        statusLed.SetRedLed(false);
-        statusLed.SetGreenLed(false);
-        statusLed.SetBlueLed(false);
+        gamepadInputChanged.AxisChanged += (s, e) =>
+            Console.WriteLine($"Axis {e.Name} ({e.Address}) Changed: {e.Value}");
     }
 }
